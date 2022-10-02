f0bd328a826660a35c326044fa2b78ead89bc949
viamoore <91388235+viamoore@users.noreply.github.com>
2022-10-02 15:52:08 -0700

    Buggy Victory Stuff

--- Assets/Scripts/PlatypusManager.cs
+++ Assets/Scripts/PlatypusManager.cs
@@ -11,37 +11,38 @@ public class PlatypusManager : MonoBehaviour
     public TextMeshProUGUI patText;
     public TextMeshProUGUI timeText;
     public Sprite []phases;
-    public SpriteRenderer spriteRender;
+    private SpriteRenderer spriteRender;
     private int spriteOffset;
     // How many times all the images in a phase are cycled through before moving on to the next phase
     public int cyclesPerPhase;
     // How many sprites are cycled through within a phase
     public int spritesPerCycle;
     private int patsPerPhase;
+    private MicrogameJamController microgameJamController;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("MicrogameJamController").GetComponent<MicrogameJamController>().SetMaxTimer(15);
+        microgameJamController = GameObject.Find("MicrogameJamController").GetComponent<MicrogameJamController>();
+        microgameJamController.SetMaxTimer(15);
         spriteRender = GameObject.Find("Paper").GetComponent<SpriteRenderer>();
         pats = 0;
         patsPerPhase = cyclesPerPhase * spritesPerCycle;
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if(pats >= winCondition){
-           //play platypus explsion animation
-
+        float time = microgameJamController.GetTimer();
+        timeText.text = "Time: " + time;
+        if (time <= .1 && pats >= winCondition) {
+            microgameJamController.WinGame();
         }
+    }  
 
-        //timeText.text = "Time: " +GameObject.Find("MicrogameJamController").GetComponent<MicrogameJamController>().GetTimer();
-
-    }
     public void UpdatePats(int patsToAdd){
         pats++;
         patText.text = "Pats: " + pats;
-        if (pats>1 && pats%patsPerPhase==1) spriteOffset++;
+        if (pats>1 && pats%patsPerPhase==1) spriteOffset ++;
         spriteRender.sprite = phases[(pats)%3+spriteOffset];
     }
 }
