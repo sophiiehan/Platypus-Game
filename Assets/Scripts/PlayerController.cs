59bfa4883062422a4d08b1f5d712fe636fcd201d
Sophie Han <32656696+SophieTheMushroom@users.noreply.github.com>
2022-10-01 18:14:34 -0700

    Added space bar patting mechanic, put the hand image on the capsule,  set X and y Bounds for the hand movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,19 +8,42 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public float horizontalInput;
     public float verticalInput;
+    public float patDistance;
+
+    public float xBoundLeft;
+    public float xBoundRight;
+    public float yBound;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        patDistance = 10;
+        speed = 10;
+        xBoundLeft = -6;
+        xBoundRight = 13;
+        yBound = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        if(transform.position.x >xBoundLeft && x<xBoundRight){
+            horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        }
+        
+        if(transform.position.y > -yBound && transform.position.y < yBound){
+            verticalInput = Input.GetAxis("Vertical");
+            transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+        }
+       
 
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.up * Time.deltaTime * speed * verticalInput);
+        if(Input.GetKeyDown(KeyCode.Space)){
+            transform.Translate(new Vector3(0,-patDistance, 0));
+        }
+        if(Input.GetKeyUp(KeyCode.Space)){
+            transform.Translate(new Vector3(0,patDistance, 0));
+
+        }
     }
 }
